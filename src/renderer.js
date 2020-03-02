4e8aa96b15758dbf7ce53361995ee60a51ba9ed1
Scott Davis <stdavis@utah.gov>
2020-03-02 12:15:52 -0700

    feat: read/write CSVs

--- src/renderer.js
+++ src/renderer.js
@@ -1,31 +1,40 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-
 import './index.css';
+import { remote } from 'electron';
+import parse from 'csv-parse/lib/sync';
+import fs from 'fs';
+import stringify from 'csv-stringify/lib/sync';
+import path from 'path';
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+
+window.openFile = () => {
+  console.log('openFile');
+  const filePaths = remote.dialog.showOpenDialogSync();
+  console.log('filePaths', filePaths);
+
+  const csvPath = filePaths[0];
+  const outputFilePath = `${path.dirname(csvPath)}/${path.basename(csvPath, '.csv')}_Attributed.csv`;
+  console.log('outputFilePath', outputFilePath);
+
+  // read records
+  const fileContents = fs.readFileSync(csvPath, 'utf8');
+  const records = parse(fileContents, {
+    columns: true,
+    skip_empty_lines: true
+  });
+  console.log(records);
+
+  const newRecords = records.map(record => {
+    return {
+      ...record,
+      test: 'a'
+    };
+  });
+  // match records
+
+  // point-in-polygon with sgid table
+
+  // write to new file with additional field name
+
+  const outputFileContents = stringify(newRecords, { header: true });
+  fs.writeFileSync(outputFilePath, outputFileContents);
+};
