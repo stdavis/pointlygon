cfd3de8236ca943ac6ebcf471ccd83f8e7b5ba1f
Scott Davis <stdavis@utah.gov>
2020-03-02 13:25:33 -0700

    feat: simple geocoding functionality

--- src/renderer.js
+++ src/renderer.js
@@ -5,8 +5,21 @@ import fs from 'fs';
 import stringify from 'csv-stringify/lib/sync';
 import path from 'path';
 
+const getParameters = () => {
+  const inputNodes = document.getElementsByTagName('input');
+  const parameters = {};
 
-window.openFile = () => {
+  for (let node of inputNodes) {
+    parameters[node.id] = node.value;
+  };
+  console.log('parameters', parameters);
+
+  return parameters;
+};
+
+const { apiKey, tableName, fieldName, inputId, inputStreet, inputZone } = getParameters();
+
+window.openFile = async () => {
   console.log('openFile');
   const filePaths = remote.dialog.showOpenDialogSync();
   console.log('filePaths', filePaths);
@@ -23,18 +36,57 @@ window.openFile = () => {
   });
   console.log(records);
 
+  const recordsForGeocoding = etlRecordsForGeocoding(records, inputId, inputStreet, inputZone);
+  const response = await fetch(`https://api.mapserv.utah.gov/api/v1/geocode/multiple?apiKey=${apiKey}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      addresses: recordsForGeocoding
+    })
+  });
+  const responseJson = await response.json();
+
+  const geocodingResults = convertGeocodingResultsToLookup(responseJson.result.addresses);
+
   const newRecords = records.map(record => {
     return {
       ...record,
-      test: 'a'
+      ...geocodingResults[record[inputId]]
     };
   });
-  // match records
-
   // point-in-polygon with sgid table
 
   // write to new file with additional field name
 
   const outputFileContents = stringify(newRecords, { header: true });
   fs.writeFileSync(outputFilePath, outputFileContents);
+};
+
+const etlRecordsForGeocoding = (records, idField, streetField, zoneField) => {
+  return records.map(record => {
+    return {
+      id: record[idField],
+      street: record[streetField],
+      zone: record[zoneField]
+    };
+  });
+};
+
+const convertGeocodingResultsToLookup = (results) => {
+  const lookup = {};
+  results.forEach(result => {
+    lookup[result.id] = {
+      x: result.location.x,
+      y: result.location.y,
+      score: result.score,
+      locator: result.locator,
+      matchAddress: result.matchAddress,
+      standardizedAddress: result.standardizedAddress,
+      addressGrid: result.addressGrid
+    }
+  });
+
+  return lookup;
 };
